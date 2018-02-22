146de66dfef1896b4b56cccbf6bc44c78dc1e597
Tobias Wältken <tobias.w@ltken.de>
2018-02-22 08:44:17 +0100

    Add the neccessary imports

--- total_tolles_ferleihsystem/db_models/__init__.py
+++ total_tolles_ferleihsystem/db_models/__init__.py
@@ -19,7 +19,7 @@ DB_COMMAND_LOGGER.addHandler(handler)
 STD_STRING_SIZE = 255
 
 
-from . import blacklist, item, itemType
+from . import blacklist, item, itemType, attribute
 
 
 @app.cli.command('create_db')
