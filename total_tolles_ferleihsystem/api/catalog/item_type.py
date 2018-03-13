1bf29424ee25ed12864e666ca84e593f37e38293
Tobias Wältken <tobias.w@ltken.de>
2018-03-13 05:14:00 +0100

    Remove all linter warnings of item_type

--- total_tolles_ferleihsystem/api/catalog/item_type.py
+++ total_tolles_ferleihsystem/api/catalog/item_type.py
@@ -1,31 +1,44 @@
-from flask import url_for, request
+"""
+This module contains all API endpoints for the namespace 'item_type'
+"""
+
+from flask import request
 from flask_restplus import Resource, abort, marshal
-from flask_jwt_extended import jwt_required, get_jwt_claims
 from sqlalchemy.exc import IntegrityError
 
 from .. import api as api
 from ..models import item_type_get, item_type_post
-from ... import jwt, db
+from ... import db
 
 from ...db_models.itemType import ItemType
 
-PATH: str= '/catalog/item_types'
-ns = api.namespace('item_type', description='ItemTypes', path=PATH)
+PATH: str = '/catalog/item_types'
+ANS = api.namespace('item_type', description='ItemTypes', path=PATH)
 
 
-@ns.route('/')
+@ANS.route('/')
 class ItemTypeList(Resource):
-    """Authentication Routes Hal resource."""
+    """
+    Item types root element
+    """
 
     @api.doc(security=None)
     @api.marshal_list_with(item_type_get)
+    # pylint: disable=R0201
     def get(self):
+        """
+        Get a list of all item types currently in the system
+        """
         return ItemType.query.all()
 
     @api.doc(security=None)
-    @ns.doc(model=item_type_get, body=item_type_post)
-    @ns.response(409, 'Name is not Unique.')
+    @ANS.doc(model=item_type_get, body=item_type_post)
+    @ANS.response(409, 'Name is not Unique.')
+    # pylint: disable=R0201
     def post(self):
+        """
+        Add a new item type to the system
+        """
         new = ItemType(**request.get_json())
         try:
             db.session.add(new)
@@ -37,17 +50,28 @@ class ItemTypeList(Resource):
                 abort(409, 'Name is not unique!')
             abort(500)
 
-@ns.route('/<int:id>/')
+@ANS.route('/<int:id>/')
 class ItemTypeDetail(Resource):
+    """
+    Single item type element
+    """
 
     @api.doc(security=None)
     @api.marshal_with(item_type_get)
-    def get(self, id):
-        return ItemType.query.filter(ItemType.id == id).first()
-    
-    @ns.response(404, 'Item type not found.')
-    def delete(self, id):
-        item_type = ItemType.get_by_id(id)
+    # pylint: disable=R0201
+    def get(self, type_id):
+        """
+        Get a single item type object
+        """
+        return ItemType.query.filter(ItemType.id == type_id).first()
+
+    @ANS.response(404, 'Item type not found.')
+    # pylint: disable=R0201
+    def delete(self, type_id):
+        """
+        Delete a item type object
+        """
+        item_type = ItemType.get_by_id(type_id)
         if item_type is None:
             abort(404, 'Requested item type was not found!')
         db.session.delete(item_type)
