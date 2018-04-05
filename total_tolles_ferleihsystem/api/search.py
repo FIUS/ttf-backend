5e429be762666394c49e4cdd85c8e645bdfa8612
Tobias Wältken <tobias.w@ltken.de>
2018-04-05 22:51:59 +0200

    Add deleted flag specific search

--- total_tolles_ferleihsystem/api/search.py
+++ total_tolles_ferleihsystem/api/search.py
@@ -14,20 +14,32 @@ class Search(Resource):
     Search Resource
     """
     @api.doc(security=None)
+    @api.param('search', 'the string to search for', type=str, required=False, default='')
+    @api.param('limit', 'limit the amount of return values', type=int, required=False, default=1000)
+    @api.param('tag', 'Only show items with a tag of the given tag id', type=int, required=False, default='')
+    @api.param('type', 'Only show items with the given type id', type=int, required=False, default='')
+    @api.param('deleted', 'If true also search deleted items', type=bool, required=False, default=False)
     @api.marshal_list_with(ITEM_GET)
     def get(self):
         search_string = request.args.get('search', default='', type=str)
         limit = request.args.get('limit', default=1000, type=int)
         tags = request.args.getlist('tag', type=int)
-        type = request.args.get('type', default=-1, type=int)
+        item_type = request.args.get('type', default=-1, type=int)
+        deleted = request.args.get('deleted', default=False, type=lambda x: x == 'true')
 
         search_result = Item.query.filter(Item.name.like('%' + search_string + '%'))
 
-        if len(tags) > 0:
+        if not deleted:
+            search_result = search_result.filter(not Item.deleted)
+
+        if tags:
             search_result = search_result.join(ItemToTag.item)
             search_result = search_result.filter(ItemToTag.tag_id.in_(tags))
 
-        if type != -1:
-            search_result = search_result.filter(Item.type_id == type)
+        if item_type != -1:
+            search_result = search_result.filter(Item.type_id == item_type)
 
-        return search_result.limit(limit).all()
+        test = search_result.limit(limit).all()
+        print(deleted)
+
+        return test
