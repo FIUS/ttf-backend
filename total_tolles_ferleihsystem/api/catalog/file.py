8b7066cbf78a9b6da5ca794eab7b74776feddf76
Tobias Wältken <tobias.w@ltken.de>
2018-05-18 16:08:48 +0200

    Add the basic upload endpoint

--- total_tolles_ferleihsystem/api/catalog/file.py
+++ total_tolles_ferleihsystem/api/catalog/file.py
@@ -2,9 +2,13 @@
 This module contains all API endpoints for the namespace 'file'
 """
 
-from flask_restplus import Resource
+import os
+from flask import request
+from flask_restplus import Resource, abort
+from werkzeug.utils import secure_filename
 
-from .. import API
+from .. import API, APP
+
 
 PATH: str = '/catalog/files'
 ANS = API.namespace('file', description='The file Endpoints', path=PATH)
@@ -26,7 +30,15 @@ class FileList(Resource):
         """
         Create a file
         """
-        pass
+        if 'file' not in request.files:
+            abort(400)
+        file = request.files['file']
+        if file:
+            if file.filename == '':
+                abort(400)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(APP.config['DATA_DIRECTORY'], filename))
+            return 'Success'
 
 
 @ANS.route('/<int:file_id>/')
@@ -50,5 +62,21 @@ class FileDetail(Resource):
     def put(self, file_id):
         """
         Replace a file object
+        """
+        pass
+
+
+PATH2: str = '/file-store'
+ANS2 = API.namespace('file', description='The download Endpoints to get the actual stored files', path=PATH)
+
+@ANS2.route('/<string:hash>/')
+class FileData(Resource):
+    """
+    The endpoints to get the actual stored file
+    """
+
+    def get(self, hash):
+        """
+        Get the actual file
         """
         pass
