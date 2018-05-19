be8189e95c1db0c7ed924ca09db4a6f2b32364ee
Tobias Wältken <tobias.w@ltken.de>
2018-05-19 20:04:18 +0200

    Add minor refactoring

--- total_tolles_ferleihsystem/file_store.py
+++ total_tolles_ferleihsystem/file_store.py
@@ -7,26 +7,29 @@ import hashlib
 from . import APP
 
 
-TMP_FILE_NAME = 'tmp.upload'
+TMP_FILE_NAME = 'ttf.upload'
+
+TMP_FOLDER_PATH = APP.config['TMP_DIRECTORY']
+DATA_FOLDER_PATH = APP.config['DATA_DIRECTORY']
 
 
 def save_file(file):
     """
     Save a file from a flask endpoint
     """
-    path = os.path.join(APP.config['DATA_DIRECTORY'], TMP_FILE_NAME)
+    path = os.path.join(TMP_FOLDER_PATH, TMP_FILE_NAME)
     file.save(path)
     with open(path, 'rb') as tmp_file:
         # pylint: disable=E1101
         file_hash = hashlib.sha3_256(tmp_file.read()).hexdigest()
-    os.rename(path, os.path.join(APP.config['DATA_DIRECTORY'], file_hash))
+    os.rename(path, os.path.join(DATA_FOLDER_PATH, file_hash))
 
     return file_hash
 
 
 def read_file(file_hash):
     """
-    Readpa file in the store via it's hash
+    Read a file in the store via it's hash
     """
-    with open(os.path.join(APP.config['DATA_DIRECTORY'], file_hash), 'rb') as file:
+    with open(os.path.join(DATA_FOLDER_PATH, file_hash), 'rb') as file:
         return file.read()
