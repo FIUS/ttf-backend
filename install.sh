763b06cdb0caac01268ecfe901f5c32ca82d5ab1
Tobias Wältken <tobias.w@ltken.de>
2018-07-10 20:17:15 +0200

    Start the work on the install file

--- install.sh
+++ install.sh
@@ -12,12 +12,10 @@ VENV_FOLDER=total-tolles-ferleihsystem
 NPM_BUILD_SCRIPT=build-docker
 
 CONFIG_FILE=/etc/${NAME}.conf
+WSGI_FILE=/var/www/${NAME}.wsgi
+
 LOG_PATH=/var/log/$NAME
-
-HTTP_ROOT=/var/www
-WSGI_FILE=${NAME}.wsgi
-
-APACHE_CONFIG=/etc/apache2
+APACHE_CONFIG_PATH=/etc/apache2
 
 #Asset deploy url
 DEPLOY_URL=./assets/
@@ -64,44 +62,46 @@ popd
 popd
 
 
-pushd $HTTP_ROOT
-
 if [ ! -f $WSGI_FILE ]; then
-    echo "import sys" >> $WSGI_FILE
-    echo "from os import environ" >> $WSGI_FILE
-    echo "" >> $WSGI_FILE
-    echo "activate_this = '$VENV_DIR$VENV_FOLDER/bin/activate_this.py'" >> $WSGI_FILE
-    echo "with open(activate_this) as file_:" >> $WSGI_FILE
-    echo "    exec(file_.read(), dict(__file__=activate_this))" >> $WSGI_FILE
-    echo "" >> $WSGI_FILE
-    echo "sys.path.insert(0, '$SOURCE')" >> $WSGI_FILE
-    echo "environ['MODE'] = 'production'" >> $WSGI_FILE
+    echo "[INFO] Create WSGI file: $WSGI_FILE"
+    cat > $WSGI_FILE << EOF
+import sys
+from os import environ
 
-    echo -n "environ['JWT_SECRET_KEY'] = '" >> $WSGI_FILE
-    hexdump -n 32 -e '4/4 "%08X" 1 ""' /dev/urandom >> $WSGI_FILE
-    echo "'" >> $WSGI_FILE
+activate_this = '$VENV_DIR$VENV_FOLDER/bin/activate_this.py'
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
-    echo "" >> $WSGI_FILE
-    echo "from $PACKAGE import app as application" >> $WSGI_FILE
-    echo "" >> $WSGI_FILE
+sys.path.insert(0, '$SOURCE')
+environ['MODE'] = 'production'
+environ['JWT_SECRET_KEY'] = '$(hexdump -n 32 -e '4/4 "%08X" 1 ""' /dev/urandom)'
+
+from $PACKAGE import app as application
+EOF
+else
+    echo "[WARN] Can't create WSGI file: $WSGI_FILE; It already exists!"
 fi
 
-popd
 
 if [ ! -f $CONFIG_FILE ]; then
-    echo "WEBPACK_MANIFEST_PATH = '$SOURCE/$PACKAGE/build/manifest.json'" >> $CONFIG_FILE
-    echo "SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/apache.db'" >> $CONFIG_FILE
-    echo "CELERY_BROKER_URL = 'amqp://localhost'" >> $CONFIG_FILE
-    echo "CELERY_RESULT_BACKEND = 'rpc://'" >> $CONFIG_FILE
-    echo "LOG_PATH = '$LOG_PATH'" >> $CONFIG_FILE
+    echo "[INFO] Create config file: $CONFIG_FILE"
+    cat > $CONFIG_FILE << EOF
+WEBPACK_MANIFEST_PATH = '$SOURCE/$PACKAGE/build/manifest.json'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/apache.db'
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_RESULT_BACKEND = 'rpc://'
+LOG_PATH = '$LOG_PATH'
+EOF
+else
+    echo "[WARN] Can't create config file: $CONFIG_FILE; It already exists!"
 fi
+
 
 if [ ! -d $LOG_PATH ]; then
     mkdir $LOG_PATH
-    chmod a+r+w $LOG_PATH
 fi
 
-pushd $APACHE_CONFIG
+pushd $APACHE_CONFIG_PATH
 
 if [ ! -d sites-available ]; then
     mkdir sites-available
@@ -137,6 +137,4 @@ pushd $SOURCE
 popd
 
 # reload apache
-pushd $HTTP_ROOT
-    touch $WSGI_FILE
-popd
+touch $WSGI_FILE
