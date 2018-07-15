07c6a00ea526c82b3c21080e8e06f68173d0b551
Fabian Bühler <st16290@stud.uni-stuttgart.de>
2018-07-15 15:17:32 +0200

    better breadcrumbs

--- total_tolles_ferleihsystem/src/app/navigation/breadcrumbs.component.ts
+++ total_tolles_ferleihsystem/src/app/navigation/breadcrumbs.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavigationService, Breadcrumb } from './navigation-service';
+import { Observable } from 'rxjs/Rx';
+
 
 @Component({
   selector: 'ttf-breadcrumbs',
@@ -8,12 +10,30 @@ import { NavigationService, Breadcrumb } from './navigation-service';
 })
 export class BreadcrumbsComponent implements OnInit {
 
+    @ViewChild('home') home: ElementRef;
+    @ViewChild('bcContainer') bcContainer: ElementRef;
+
+    hovered: boolean = false;
+
     breadcrumbs: Array<Breadcrumb>;
 
     constructor(private data: NavigationService) { }
 
     ngOnInit(): void {
-        this.data.currentBreadcrumbs.subscribe(breadcrumbs => this.breadcrumbs = breadcrumbs);
+        this.data.currentBreadcrumbs.subscribe(breadcrumbs => {
+            this.breadcrumbs = breadcrumbs;
+            this.scrollToBottom();
+        });
+    }
+
+    breadcrumbHeight = () => {
+        return this.home.nativeElement.offsetHeight;
+    }
+
+    scrollToBottom = () => {
+        Observable.timer(50).take(1).subscribe(() => {
+            this.bcContainer.nativeElement.scrollTop = this.bcContainer.nativeElement.scrollHeight;
+        });
     }
 
 }
