7c283fdb3ba96440bcb4c5381ffd95c3c2291630
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-11-15 14:48:41 +0100

    Move ui related files to a seperate repo.
    This commit only removes files that are only relevant for the UI.
    Subsquent commits will clean the other files from UI related parts.

--- total_tolles_ferleihsystem/src/app/shared/box/box.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ttf-box',
-    templateUrl: './box.component.html',
-    styleUrls: ['./box.component.scss']
-})
-export class myBoxComponent {}
