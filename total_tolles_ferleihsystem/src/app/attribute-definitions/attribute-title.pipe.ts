7a0428488318e1801e898505117a484a9228c5d8
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2020-11-15 14:48:41 +0100

    Move ui related files to a seperate repo.
    This commit only removes files that are only relevant for the UI.
    Subsquent commits will clean the other files from UI related parts.

--- total_tolles_ferleihsystem/src/app/attribute-definitions/attribute-title.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { attrDefTitle } from './helper-functions';
-
-@Pipe({name: 'attributeDefinitionTitle'})
-export class AttributeDefinitionTitlePipe implements PipeTransform {
-    transform(value): string {
-        return attrDefTitle(value);
-    }
-}
--- total_tolles_ferleihsystem/src/app/shared/forms/question-hidden.ts
+++
@@ -1,9 +0,0 @@
-import { QuestionBase, QuestionOptions } from './question-base';
-
-export class HiddenQuestion extends QuestionBase<any> {
-    controlType = 'hidden';
-
-    constructor(options: QuestionOptions = {}) {
-        super(options);
-    }
-}
